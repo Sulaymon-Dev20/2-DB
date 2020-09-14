2eac7f53c46204eb59284ac3314d94d7ba70cc36
Khumoyun Khujamov <xojamov07@gmail.com>
2020-09-14 17:50:41 +0500

    Counting requests and saving them on specified interval

---
+++ src/main/java/uz/ssd/locationsender/config/CounterFilter.java
@@ -0,0 +1,40 @@
+package uz.ssd.locationsender.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.context.SecurityContextHolder;
+import uz.ssd.locationsender.domain.entity.User;
+
+import javax.servlet.*;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Author: Khumoyun Khujamov
+ * Date: 9/11/20
+ * Time: 5:27 PM
+ */
+public class CounterFilter implements Filter {
+    private static final Logger LOGGER = LoggerFactory.getLogger(CounterFilter.class);
+    public static Map<Long, Long> counter = new HashMap<>();
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if (user != null) {
+            Long userId = user.getId();
+            counter.put(userId, counter.containsKey(userId) ? counter.get(userId) + 1 : 1);
+            LOGGER.info("Request Count : {} with userId : {}", counter.get(userId), user.getId());
+        }
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
---
+++ src/main/java/uz/ssd/locationsender/service/DefaultUserService.java
@@ -0,0 +1,44 @@
+package uz.ssd.locationsender.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import uz.ssd.locationsender.config.CounterFilter;
+import uz.ssd.locationsender.domain.entity.User;
+import uz.ssd.locationsender.repository.UserRepository;
+
+import java.util.Date;
+import java.util.Map;
+
+/**
+ * Author: Khumoyun Khujamov
+ * Date: 9/12/20
+ * Time: 5:46 PM
+ */
+@Service
+public class DefaultUserService {
+    private final UserRepository userRepository;
+    public static final Logger LOGGER = LoggerFactory.getLogger(DefaultUserService.class);
+
+    public DefaultUserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    // This method executes in every 5 minutes
+    @Scheduled(fixedRate = 300000)
+    public void saveRequestCount() {
+
+        for (Map.Entry<Long, Long> entry : CounterFilter.counter.entrySet()) {
+            Long userId = entry.getKey();
+            User user = userRepository.findById(userId).orElseThrow(() -> new UsernameNotFoundException("User with id " + userId + "not found"));
+            Long updatedReqCount = user.getReqCount() + entry.getValue();
+            user.setReqCount(updatedReqCount);
+            user.setUpdatedDate(new Date());
+            userRepository.save(user);
+        }
+        CounterFilter.counter.clear();
+    }
+}
